ad450aa4ab63d3d128d226dcdbb8a0273e783a1b
Victor <Victor@iMac-Nest.local>
2015-07-20 17:37:58 +0300

    Initial Commit

---
+++ CameraBuffer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraBuffer
+//
+//  Created by Victor on 20.07.15.
+//  Copyright (c) 2015 Nestline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraBuffer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CameraBuffer
+//
+//  Created by Victor on 20.07.15.
+//  Copyright (c) 2015 Nestline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
